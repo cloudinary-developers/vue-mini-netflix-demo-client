764af1c0664a493d5b90274fbfd70c4a975ef127
Christian Nwamba <nwambachristian@gmail.com>
2018-03-21 09:47:27 +0100

    Video Player

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,31 @@
         </div>
       </div>
     </nav>
+
+    <VideoPlayer :cloudinaryInstance="cloudinaryInstance" :movie="movie"></VideoPlayer>
+
   </div>
 </template>
 
 
 <script>
-
+import VideoPlayer from './components/VideoPlayer.vue';
 export default {
-
-}
+  data() {
+    return {
+      movie: {},
+    }
+  },
+  created() {
+    this.cloudinaryInstance = window.cloudinary.Cloudinary.new({
+      cloud_name: 'christekh',
+      secure: true
+    });
+  },
+  components: {
+    VideoPlayer
+  }
+};
 </script>
 
 <style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="trailer-bg">
+      <video
+        id="trailer"
+        autoplay
+        loop
+        class="cld-video-player trailer-bg__video">
+      </video>
+      <div class="trailer-content">
+        <h1 class="is-size-1	has-text-weight-bold">{{movie.title || 'Black Panther'}}</h1>
+        <button class="button" @click="playPause()">
+          <i class="fa fa-play"></i> &nbsp; / &nbsp; 
+          <i class="fa fa-pause"></i> 
+          &nbsp; Trailer</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['cloudinaryInstance', 'movie'],
+  mounted() {
+    this.player = this.cloudinaryInstance.videoPlayer('trailer', {
+      controls: false,
+      transformation: { crop: 'scale', width: 1200 }
+    });
+    this.player.source(
+      this.movie.trailer ||
+        'Marvel_Studios_Black_Panther_-_Official_Trailer_nmrtr7'
+    );
+    // Mute on blur
+    document.hidden && this.player.mute();
+    window.onfocus = () => this.player.unmute();
+    window.onblur = () => this.player.mute();
+  },
+  watch: {
+    movie(newMovie, oldMovie) {
+      console.log(newMovie, oldMovie);
+      this.player.source(newMovie.trailer);
+    }
+  },
+  methods: {
+    playPause() {
+      this.paused ? this.player.play() : this.player.pause();
+    }
+  }
+};
+</script>
+
+<style>
+.trailer-bg {
+  position: relative;
+}
+
+.trailer-bg__video {
+  position: absolute;
+  width: 100%;
+  outline: none;
+}
+
+.trailer-content {
+  position: absolute;
+  top: 30%;
+  left: 200px;
+}
+
+.trailer-content button {
+  border: none;
+  border-radius: 0;
+}
+</style>
