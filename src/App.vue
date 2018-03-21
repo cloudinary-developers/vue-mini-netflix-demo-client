b4f31bb9a95a9f0df493ccf3f843dfacb86be1c7
Christian Nwamba <nwambachristian@gmail.com>
2018-03-21 07:48:31 +0100

    Nav

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <img src="./assets/logo.svg" alt="Netflix" width="112" height="28">
+          </a>
+
+           <div class="navbar-menu">
+            <div class="navbar-end">
+              <!-- Upload button here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
@@ -21,8 +31,19 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #e5e5e5;
+  background-color: #000;
+  padding-top: 30px;
+}
+
+.navbar {
+  background-color: transparent;
+  z-index: 2;
+  padding: 30px 0;
+}
+
+.navbar-item {
+  height: 25px;
+  width: auto;
 }
 </style>
